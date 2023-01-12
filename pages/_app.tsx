6ad7f0979b104d1061001e6c4268846bdcb46f53
Jaime Filho <jaimeadanielfilho@hotmail.com>
2023-01-11 22:53:27 -0300

    feat: host font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
+import { Roboto } from '@next/font/google';
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.scss'
 
+const roboto = Roboto({ 
+    weight: ["400", "700"],
+    subsets: ["latin"]
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <div className={roboto.className}>
+            <Component {...pageProps} />
+        </div>
+    );
 }
